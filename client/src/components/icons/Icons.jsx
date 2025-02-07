7e12514aef3a4744f4b85865293aaaf5c8c00655
Nishith Dubey <nishithrbd@gmail.com>
2025-02-07 22:06:12 +0530

    Create icons.jsx in components/icons/

--- client/src/components/icons/Icons.jsx
+++ client/src/components/icons/Icons.jsx
@@ -1,4 +1,14 @@
+// All images @ '/images/xyz.png or svg'
+
+// Main Page ->
+// fight.png, contract.png, headset.png, amenities.png
+// love.png, calendar.png, network.png, idea.png
+
+//Our Story page ->
 import { Home, Users, Sparkles, Layout, ShieldCheck } from "lucide-react";
+
+// Testimoial page ->
+import { Quote, Star, ArrowRight, Check } from 'lucide-react';
 
 const Icons = {
   Home,
@@ -6,6 +16,29 @@ const Icons = {
   Sparkles,
   Layout,
   ShieldCheck,
+  Quote,
+  Star,
+  ArrowRight,
+  Check,
 };
 
 export default Icons;
+
+
+// Footer ->
+// Twitter.svg, Linkedin.svg, Instagram.svg
+
+// Partner with us ->
+// brain.png, location_Marker.svg, house.png, secure.png, responsible.png
+
+// Login page
+import { EyeIcon, EyeOffIcon } from 'lucide-react';
+
+// Payment ->
+import { ChevronDownCircle, ChevronUpCircle } from 'lucide-react';
+
+// Hostel Component ->
+// /images/Wifihostel.svg
+// /images/Housekeeping.svg
+// /images/Cctv.svg
+// /images/Parking.svg
